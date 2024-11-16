e0adad189d57eadef0568fde2cc09e7cea3a0509
Isaac Roberts <isaacroberts212@gmail.com>
2024-11-15 23:38:42 -0500

    chapter 5 and 6

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController 
-{
+
+class MainController extends AbstractController
+{ 
     #[Route('/')]
+
     public function homepage(): Response
     {
-        return new Response('<strong>StarShop</strong>: stuff and things ');
+
+        $starshipCount = 457;
+
+        $myShip = [
+            'name' => 'USS Stuff and Things',
+            'class' => 'Random',
+            'captain' => 'Jack Sparrow',
+            'status' => "In Davey Jones' Locker"
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
